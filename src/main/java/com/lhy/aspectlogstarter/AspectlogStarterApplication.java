6fa413cf1a71b898187095792b61c4e5ef6f1726
liduying <lihuiyongty@163.com>
2020-09-02 20:39:15 +0800

    init project

---
+++ src/main/java/com/lhy/aspectlogstarter/AspectlogStarterApplication.java
@@ -0,0 +1,13 @@
+package com.lhy.aspectlogstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectlogStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspectlogStarterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lhy/aspectlogstarter/config/AspectLogProperties.java
@@ -0,0 +1,16 @@
+package com.lhy.aspectlogstarter.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("aspectLog")
+public class AspectLogProperties {
+    private boolean enable;
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+}
